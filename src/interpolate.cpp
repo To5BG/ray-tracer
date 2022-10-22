59268791fdacf1328264a9c800e0375b7f15d7cc
Andrei Dascalu <A.Dascalu@student.tudelft.nl>
2022-10-22 21:33:51 +0200

    Visual Debug Temp

--- src/interpolate.cpp
+++ src/interpolate.cpp
@@ -3,8 +3,11 @@
 
 glm::vec3 computeBarycentricCoord (const glm::vec3& v0, const glm::vec3& v1, const glm::vec3& v2, const glm::vec3& p)
 {
-    // TODO: implement this function.
-    return glm::vec3(0.0);
+    float totalArea = glm::length(glm::cross((v0 - v2), (v1 - v2))) / 2;
+    float alpha = (glm::length(glm::cross(p - v2, v1 - v2))) / (2 * totalArea);
+    float beta = (glm::length(glm::cross(p - v2, v0 - v2))) / (2 * totalArea);
+    float gamma = (glm::length(glm::cross(p - v1, v0 - v1))) / (2 * totalArea);
+    return glm::vec3(alpha, beta, gamma);
 }
 
 glm::vec3 interpolateNormal (const glm::vec3& n0, const glm::vec3& n1, const glm::vec3& n2, const glm::vec3 barycentricCoord)
